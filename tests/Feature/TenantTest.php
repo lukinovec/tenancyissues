6924bb73485f1c4721dda0e6d3080d4e391e8cb5
lukinovec <lukinovec@gmail.com>
2023-02-06 10:54:38 +0100

    Add Pest and some tests

---
+++ tests/Feature/TenantTest.php
@@ -0,0 +1,243 @@
+<?php
+
+use App\Models\Product;
+
+test('product exists 0', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('product exists 1', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('product exists 2', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('product exists 3', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('product exists 4', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('product exists 5', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('product exists 6', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('product exists 7', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('product exists 8', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('product exists 9', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('another product exists 0', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('another product exists 1', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('another product exists 2', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('another product exists 3', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('another product exists 4', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('another product exists 5', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('another product exists 6', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('another product exists 7', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('another product exists 8', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('another product exists 9', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('product exists 0 duplicate', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('product exists 1 duplicate', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('product exists 2 duplicate', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('product exists 3 duplicate', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('product exists 4 duplicate', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('product exists 5 duplicate', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('product exists 6 duplicate', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('product exists 7 duplicate', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('product exists 8 duplicate', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
+
+test('product exists 9 duplicate', function() {
+    expect(Product::first())->toBeNull();
+
+    $product = Product::create();
+
+    expect(Product::find($product->id))->not()->toBeNull();
+});
